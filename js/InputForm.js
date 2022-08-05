9b20d15d963c4157c7886985a7ce5b54faf19f8b
swatikanishu <nishuswatika@gmail.com>
2022-08-06 01:36:15 +0530

    Added:predefined password format check

--- js/InputForm.js
+++ js/InputForm.js
@@ -1,6 +1,7 @@
 const NAME_REGEX = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
 const EMAIL_REGEX = RegExp('^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$');
 const PHONE_REGEX = RegExp('^[0-9]{1,2}\\s{1}[0-9]{10}$');
+const PASSWORD_REGEX = RegExp('^[a-zA-Z0-9]*[@#$%^&-+=()]*[a-zA-Z0-9]{8,}$')
 const text = document.querySelector('#text');
 const textError = document.querySelector('.text-error');
 
